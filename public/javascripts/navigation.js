b1bb31a25f71ab9fa2e4bfec1526b96c76d99516
sricharan sunder <sricharan@joshsoftware.com>
2011-08-30 00:29:29 +0530

    countdown timer added .. new pic added

--- public/javascripts/navigation.js
+++ public/javascripts/navigation.js
@@ -1,4 +1,24 @@
 
+
+$(function () {
+$('#countdown').countdown({until:$.countdown.UTCDate(+6, 2012, 1 - 1, 21), format: 'DHMS', layout:
+'<div id="timer">' + '<hr />'+
+	'<div id="timer_days" class="timer_numbers">{dnn}</div>'+
+	'<div id="timer_hours" class="timer_numbers">{hnn}</div>'+
+	'<div id="timer_mins" class="timer_numbers">{mnn}</div>'+
+	'<div id="timer_seconds" class="timer_numbers">{snn}</div>'+
+'<div id="timer_labels">'+
+	'<div id="timer_days_label" class="timer_labels">days</div>'+
+	'<div id="timer_hours_label" class="timer_labels">hours</div>'+
+	'<div id="timer_mins_label" class="timer_labels">mins</div>'+
+	'<div id="timer_seconds_label" class="timer_labels">secs</div>'+
+'</div>'+
+'</div>'
+});
+});
+
+
+
 $(document).ready( function(){
 
 $('.has_submenu').hover( function() {
@@ -52,4 +72,6 @@ function rotate() {
 	.removeClass('show');
 	
 };
+
+
 
