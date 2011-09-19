cf1135a52a0092751267ba602dcb8304f19689d3
Sricharan Sunder <sricharan92@gmail.com>
2011-09-19 19:45:34 +0530

    Removed unnecessary routes previously used for AJAX content rendering

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,6 @@ MUNsite::Application.routes.draw do
   resources :user
   
   match "/index", :to => "home#index", :as => "home_index"
-  match "/home" => "home#home_content", :as => "home_content"
-  match "/registration_content" => "home#registration_content", :as => "registration_content"
-  match "/mun_conference_content" => "home#mun_conference_content", :as => "mun_conference_content"
-  match "/delegate_registrations" => "home#delegate_registrations", :as => "delegate_registrations"
-  match "/delegates" => "home#delegates", :as => "delegates"
-  match "/ip_registrations" => "home#ip_registrations", :as => "ip_registrations"
   match "/executive_board_registrations" => "home#eb_registrations", :as => "eb_registrations"
   match "/executive_board" => "home#executive_board", :as => "executive_boards"
   # The priority is based upon order of creation:
