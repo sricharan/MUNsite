69800a6ee259cfbe46d622b4cfac6470e9d4b1ef
Sricharan Sunder <sricharan92@gmail.com>
2011-09-30 23:37:29 +0530

    error fixed

--- app/models/international_press.rb
+++ app/models/international_press.rb
@@ -1,4 +1,4 @@
-ngrid Bergmanclass InternationalPress < ActiveRecord::Base
+class InternationalPress < ActiveRecord::Base
   has_one :user, :as => :resource, :dependent => :destroy
   accepts_nested_attributes_for :user
 end
