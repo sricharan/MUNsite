d2dee83c234f445030469fe6d5937afc82ba3587
Sricharan Sunder <sricharan92@gmail.com>
2011-10-14 21:59:08 +0530

    migrations added again

--- db/migrate/20111006180012_add_council_preference_to_executive_boards.rb
+++
@@ -1,9 +0,0 @@
-class AddCouncilPreferenceToExecutiveBoards < ActiveRecord::Migration
-  def self.up
-    add_column :executive_boards, :council_preference, :string
-  end
-
-  def self.down
-    remove_column :executive_boards, :council_preference
-  end
-end
--- db/migrate/20111006180101_add_position_preference_to_executive_boards.rb
+++
@@ -1,9 +0,0 @@
-class AddPositionPreferenceToExecutiveBoards < ActiveRecord::Migration
-  def self.up
-    add_column :executive_boards, :position_preference, :string
-  end
-
-  def self.down
-    remove_column :executive_boards, :position_preference
-  end
-end
