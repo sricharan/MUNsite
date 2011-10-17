aeb27c135bd45e67b283fa730dbdf3b629eeda03
Sricharan Sunder <sricharan92@gmail.com>
2011-10-17 05:57:29 +0530

    slideshows changed and new slideshow for sponsors added

--- public/javascripts/navigation.js
+++ public/javascripts/navigation.js
@@ -21,7 +21,20 @@ $('#countdown').countdown({until:$.countdown.UTCDate(+6, 2012, 1 - 1, 21), forma
 
 $(document).ready( function(){
 
+$('#slider').nivoSlider({
 
+  animSpeed:500,
+  pauseTime:5000
+});
+$('#slider_two').nivoSlider({
+  
+  effect: 'fade',
+  directionNav: false,
+  controlNav:false,
+  animSpeed:500,
+  pauseTime:5000
+
+});
 window.scrollTo(0,500);
 $('.has_submenu').hover( function() {
 
@@ -38,42 +51,8 @@ $("#logo").hover( function() {
 
   $('.submenu').hide();});
 
-$(".rotator").hover( function() {
+$("#slider").hover( function() {
 
   $('.submenu').hide();});
 
- theRotator();
- $('div.rotator').fadeIn(1000);
- $('div.rotator ul li').fadeIn(1000);
 });
-
-function theRotator() {
-
-  $("div.rotator ul li").css({opacity: 0.0});
-  $("div.rotator ul li:first").css({opacity:1.0});
-
-  setInterval('rotate()', 6000);
-
-}
-
-function rotate() {	
-	var current = ($('div.rotator ul li.show')?  $('div.rotator ul li.show') : $('div.rotator ul li:first'));
-
-    if ( current.length == 0 ) current = $('div.rotator ul li:first');
-
-	var next = ((current.next().length) ? ((current.next().hasClass('show')) ? $('div.rotator ul li:first') :current.next()) : $('div.rotator ul li:first'));
-	
-	
-			
-
-	next.css({opacity: 0.0})
-	.addClass('show')
-	.animate({opacity: 1.0}, 1000);
-
-	current.animate({opacity: 0.0}, 1000)
-	.removeClass('show');
-	
-};
-
-
-
